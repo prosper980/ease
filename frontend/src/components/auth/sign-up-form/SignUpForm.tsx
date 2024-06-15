5618821afc77e2744b765d1c2589642f1c699313
Prosper Awul <atokwame42@gmail.com>
2024-06-15 23:34:04 +0000

    frontend:implemented password mismatch error style

--- frontend/src/components/auth/sign-up-form/SignUpForm.tsx
+++ frontend/src/components/auth/sign-up-form/SignUpForm.tsx
@@ -1,8 +1,30 @@
 import { Form, useActionData } from "react-router-dom";
+import { useRef } from "react";
+import { useState } from "react";
 
 const SignUpForm = () => {
 
+    // router hooks
     const errors = useActionData();
+    
+    // react hooks
+    const [errorStyle, setErrorStyle] = useState("border border-slate-300 focus:outline-none focus:border-slate-400 focus:ring-1 focus:ring-slate-500");
+    const [passworMisMatchMessage, setPasswordMisMatchMessage] = useState("");
+    const firstPasswordFieldRef = useRef<HTMLInputElement>(null);
+    const secondPasswordFieldRef = useRef<HTMLInputElement>(null);
+
+
+    function passwordFieldValueOnclickHandler(){
+
+        if(secondPasswordFieldRef.current?.value !== firstPasswordFieldRef.current?.value){
+            setErrorStyle("border border-red-400 focus:outline-none focus:border-red-400 focus:ring-1 focus:ring-red-400");
+            setPasswordMisMatchMessage("Passwords do not match");
+        }else{
+            setErrorStyle("border border-slate-300 focus:outline-none focus:border-slate-400 focus:ring-1 focus:ring-slate-500");
+            setPasswordMisMatchMessage("");
+   
+        }
+    }
 
     return(
         <>
@@ -42,27 +64,28 @@ const SignUpForm = () => {
                                 <div>
                                     <label htmlFor="first-password">
                                         <span className="block text-sm font-medium text-slate-600 font-plus-font"> Password </span>
-                                        <input className="block w-full mt-1 px-3 py-2 bg-white border text-slate-700 border-slate-300 text-sm shadow-sm placeholder-slate-400
-                                            focus:outline-none focus:border-slate-400 focus:ring-1 focus:ring-slate-500" type="password" name="new_user_first_password" id="first-password" placeholder="Choose a password" required/>
+                                        <input className={`block w-full mt-1 px-3 py-2 bg-white text-slate-700  text-sm shadow-sm placeholder-slate-400
+                                            ${errorStyle}`} ref={firstPasswordFieldRef} type="password" name="new_user_first_password" id="first-password" placeholder="Choose a password" required/>
                                     </label>
                                 </div>
 
-                                <div>
-                                    {typeof errors === 'string' && <span className="text-xs text-red-500" > {errors} </span>}
-                                </div>
+                                
+                                
 
                                 <div>
                                     <label htmlFor="second-password">
                                         <span className="block text-sm font-medium text-slate-600 font-plus-font"> Confirm Password </span>
-                                        <input className="block w-full mt-1 px-3 py-2 bg-white border text-slate-700 border-slate-300 text-sm shadow-sm placeholder-slate-400
-                                            focus:outline-none focus:border-slate-400 focus:ring-1 focus:ring-slate-500" type="password" name="new_user_second_password" id="second-password" placeholder="Repeat password" required/>
+                                        <input className={`block w-full mt-1 px-3 py-2 bg-white text-slate-700  text-sm shadow-sm placeholder-slate-400
+                                            ${errorStyle}`} onChange={passwordFieldValueOnclickHandler} ref={secondPasswordFieldRef} type="password" name="new_user_second_password" id="second-password" placeholder="Repeat password" required/>
                                     </label>
                                 </div>
 
+
+                                {(typeof errors === 'string') && <span className="text-xs text-red-500">{errors}</span>}
+                                {<span className="text-xs text-red-500">{passworMisMatchMessage}</span>}
                                 <div>
                                     <button className="w-full bg-slate-900 text-white text-center p-2 hover:bg-slate-800 font-plus-font" type="submit"> Submit </button>
                                 </div>
-
 
                             </Form>
 
